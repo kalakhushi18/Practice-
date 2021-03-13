f553d1c2d526a96bf5204ec75bdfc8e579bd38bd
kalakhushi18 <khushikala18@gmail.com>
2021-03-13 22:10:00 +0530

    Context

---
+++ src/components/ComponentC.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { UserConsumer } from './userContext'
+
+ class ComponentC extends Component {
+    render() {
+        return (
+           <UserConsumer>
+               {
+                   (username)=>{
+                       return <h2>Hello {username} </h2>
+                   }
+               }
+           </UserConsumer>
+        )
+    }
+}
+
+export default ComponentC
