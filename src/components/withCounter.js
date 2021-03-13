9c8934556edf2767c62b2166543bd094bc9eddbe
kalakhushi18 <khushikala18@gmail.com>
2021-03-13 22:11:31 +0530

    Next commit

---
+++ src/components/withCounter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Update = (originalvalue)=>{
+ class Newvalue extends React.Component{
+     render( ){
+         return originalvalue
+     }
+ }
+return Newvalue
+}
+
+// {...this.props }
+//hoc me params bhi pass ho skte 
